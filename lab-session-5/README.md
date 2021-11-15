e553282427171745bb3d116857a4ce05dae6c044
Adham Khatean <aekhatean@gmail.com>
2021-11-15 21:04:59 +0200

    Present lab session 5 work results.

---
+++ lab-session-5/README.md
@@ -0,0 +1,21 @@
+Day 2 Lab Assignments
+
+Please do these assignments:
+
+Create array that contain some tips about JavaScript (Array of 10 strings, each string is tip about JS), and show random tip for the user each time he opens the page "Tip of the day".
+
+Make a button that display the current date and time in local format on the page.
+
+Use Try catch and onError to handle any errors in your page.
+
+In the assignment No.2, ask the user to choose the color he want to use to display the date (Give him choices: (r) red, (b) blue, (g) green) and he enters the letter of color and use associative array to map his entered letter to color (if he entered r, use red color,…etc), and then display this text: "Current Date:" in bold font, and next to it the current date value in local format using the color he choose. (Don’t use HTML tags, and don't use if or switch to select the color).
+
+Bonus assignments:
+
+In assignment No.4, make the function if user entered date format is correct initialize the date object with this date and make the function return the date object, and if it isn’t in correct format throw an exception, and use another function to call the previous function and handle this exception using (Try Catch), and show alert that shows description for the error if the date entered by user isn’t valid.
+
+Try to handle the previous exception using onError Event.
+
+Make function to sort array numerically in JavaScript, and explain how does it work?
+
+Show prompt that ask user to enter the date of today and tell user to enter the date in the following format (DD – MM – YYYY) ex. 22–01–1999, and then create function that take user input as a parameter and ensure that the string is entered in this format (that user entered string is 10 characters and contains (-) after the second character and after fifth character). If the user input was correct: make the function create new date object, and initialize it with Day, Month, year values (using date constructor: Date(y,m,d)) and then show alert to the user with the date in date string format. If user input wasn't correct, show alert saying "Wong Date Format". Create button “show date” that calls the function.
---
+++ lab-session-5/index.js
@@ -0,0 +1,112 @@
+"use strict";
+
+const jsTips = [
+  "Get your foundations right.",
+  "Don’t forget var keyword when assigning a variable’s value for the first time.",
+  "use === instead of ==",
+  "undefined, null, 0, false, NaN, '' (empty string) are all falsy.",
+  "Use Semicolons for line termination",
+  "Don’t use delete to remove an item from array, use splice() instead.",
+  "You can empty an array by setting its length to 0.",
+  "Use map() to perform a function over array elements",
+  "Use try {}... catch {} whenever possible, and put it to a good use to not confuse your users",
+  "Check the properties of an object when using a for-in loop to avoid unexpected errors.",
+];
+
+const colorPal = Array();
+colorPal["r"] = "red";
+colorPal["g"] = "green";
+colorPal["b"] = "blue";
+
+const boilerPlate = {
+  a: "character",
+  b: "also a character",
+  c: "still a character",
+  d: "what do think?",
+};
+
+const generateRandomNumberInRange = (max, min = 0) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const showRandomJsTip = () => {
+  const luckyWheel = generateRandomNumberInRange(jsTips.length - 1);
+  const randomTip = jsTips[luckyWheel];
+  alert(randomTip);
+};
+
+const showDateInLocalFormat = () => {
+  let output = undefined;
+  currDate = new Date().toLocaleDateString();
+  const color = prompt(
+    "Select a color yoy want to see your date in: (r) red, (b) blue, (g) green"
+  );
+  const cdt = "Current Date:".bold();
+  output = colorPal[color]
+    ? `${cdt} ${currDate.fontcolor(colorPal[color])}`
+    : `${cdt} ${currDate}`.fontcolor("black");
+  document.write(output);
+};
+
+const intentionallyWrong = () => {
+  try {
+    alert(boilerPlate.f.desc);
+  } catch {
+    alert("This character is out of range");
+  }
+};
+
+// showRandomJsTip();
+// intentionallyWrong();
+
+/**
+ * Bonus Task
+ */
+
+// Prompt user for Date and handle wrong formats
+const checkUserDateInput = (date) => {
+  if (date.length === 10 && date[2] === "-" && date[5] === "-") {
+    const [d, m, y] = date.split("-");
+    const userDate = new Date(y, m, d).toLocaleDateString();
+    alert(userDate);
+    return userDate;
+  } else {
+    try {
+      alert("Error: Wrong Date Format");
+      throw "Wrong Date Format";
+    } catch (error) {
+      console.error(error);
+      promptUserForDate();
+    }
+  }
+};
+
+const promptUserForDate = () => {
+  const userDate = prompt(
+    "Enter today's date in this format (DD – MM – YYYY):"
+  );
+  checkUserDateInput(userDate);
+};
+
+// Create a function to sort an array of numbers
+const promptForNumbers = () => {
+  const numbersString = prompt(
+    "Enter numbers to be sorted seperated spaces, like 1,2,4,5,6:"
+  );
+  let numbersInt = Array();
+  const numbers = numbersString.split(" ");
+  numbers.map((number) => {
+    numbersInt.push(parseInt(number));
+  });
+  console.log(numbersInt);
+
+  numbersInt.forEach((number) => {
+    if (isNaN(number)) {
+      numbersInt.splice(numbersInt.indexOf(number), 1);
+    }
+  });
+  numbersInt.sort((a, b) => a - b);
+  console.log(numbersInt);
+};
+
+promptUserForDate();
